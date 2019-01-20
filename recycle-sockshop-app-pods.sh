e90f9cbec0a6ca9bb84b493ce51e707e1b86705a
steve-caron-dynatrace <steve.caron@dynatrace.com>
2019-01-20 14:46:31 -0500

    Changed dynatrace annotations, increases memory limits for some pods, added canary, adjusted redeploy scripts to use annotation

--- recycle-sockshop-app-pods.sh
+++ recycle-sockshop-app-pods.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-oc patch deployment carts -p '{"spec":{"replicas":0}}'
-oc patch deployment catalogue -p '{"spec":{"replicas":0}}' 
-oc patch deployment front-end -p '{"spec":{"replicas":0}}' 
-oc patch deployment orders -p '{"spec":{"replicas":0}}' 
-oc patch deployment payment -p '{"spec":{"replicas":0}}' 
-oc patch deployment queue-master -p '{"spec":{"replicas":0}}' 
-oc patch deployment rabbitmq -p '{"spec":{"replicas":0}}' 
-oc patch deployment shipping -p '{"spec":{"replicas":0}}' 
-oc patch deployment user -p '{"spec":{"replicas":0}}'
+oc patch deployment carts -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment catalogue -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment front-end -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment orders -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment payment -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment queue-master -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment rabbitmq -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment shipping  -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
+oc patch deployment user -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "true"}}}}}'
 
 sleep 20s
 oc get po -o wide
-
-oc patch deployment carts -p '{"spec":{"replicas":1}}'
-oc patch deployment catalogue -p '{"spec":{"replicas":1}}' 
-oc patch deployment front-end -p '{"spec":{"replicas":1}}'
-oc patch deployment orders -p '{"spec":{"replicas":1}}'
-oc patch deployment payment -p '{"spec":{"replicas":1}}'
-oc patch deployment queue-master -p '{"spec":{"replicas":1}}'
-oc patch deployment rabbitmq -p '{"spec":{"replicas":1}}'
-oc patch deployment shipping -p '{"spec":{"replicas":1}}'
-oc patch deployment user -p '{"spec":{"replicas":1}}'
-
-sleep 20s
-oc get po -o wide
-
