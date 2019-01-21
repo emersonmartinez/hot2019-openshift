07ce09b4336951d2c093b8ec21f82be11d42a86b
steve-caron-dynatrace <steve.caron@dynatrace.com>
2019-01-21 17:14:09 -0500

    added blog-app

--- recycle-sockshop-app-pods-to-uninstrument.sh
+++ recycle-sockshop-app-pods-to-uninstrument.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+oc project dev
+
 oc patch deployment carts -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
 oc patch deployment catalogue -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
 oc patch deployment front-end -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
@@ -10,5 +12,17 @@ oc patch deployment rabbitmq -p '{"spec":{"template":{"metadata":{"annotations":
 oc patch deployment shipping  -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
 oc patch deployment user -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
 
+oc project production
+
+oc patch deployment carts -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment catalogue -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment front-end.stable -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment orders -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment payment -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment queue-master -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment rabbitmq -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment shipping  -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+oc patch deployment user -p '{"spec":{"template":{"metadata":{"annotations":{"dynatrace/instrument": "false"}}}}}'
+
 sleep 20s
-oc get po -o wide
+oc get po --all-namespaces -l product=sockshop
