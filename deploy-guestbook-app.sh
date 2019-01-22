61d655c8eadda7b97c42beed3c3ea1ef49a3b3e4
steve-caron-dynatrace <steve.caron@dynatrace.com>
2019-01-22 15:08:56 -0500

    added the permission grant for the onagent to import labels and annotations for the guestbook namespace

--- deploy-guestbook-app.sh
+++ deploy-guestbook-app.sh
@@ -6,3 +6,5 @@ oc adm policy add-scc-to-user anyuid system:serviceaccount:guestbook:default
 
 oc apply -f guestbook-app/
 oc expose svc/frontend
+
+oc -n guestbook policy add-role-to-user view -z default
